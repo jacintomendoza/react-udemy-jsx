7867b9826318fa54c3080f14108b1dcd3c8e05b3
Josh Mendoza <jacinto.josh.mendoza@gmail.com>
2024-02-07 19:18:45 -0600

    initial

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+////////////////// 1) Import React and ReactDOM libraries ////////////////////////////////////////////////////////////////////////
+
+import React from 'react'; // defines components and how they work together
+import ReactDOM from 'react-dom/client'; // set of componets to HTML and show on screen
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+////////////////// 2) Get a reference to the div with ID root ////////////////////////////////////////////////////////////////////////
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const el = document.getElementById('root');
+
+////////////////// 3) Tell React to take control of that element ////////////////////////////////////////////////////////////////////////
+
+const root = ReactDOM.createRoot(el);
+
+////////////////// 4) Create a component ////////////////////////////////////////////////////////////////////////
+
+// function App() {
+//     <h4>This is an element that doesn't show up!</h4>
+    
+//     // let message = 'Bye there!';
+//     // if (Math.random() > .5) {
+//     //     message = 'Hello World!';
+//     // }
+
+//     // const message = {}; // error here
+//     // const message = true; // nothing here
+//     // return <h1>{message}</h1>
+
+//     // const date = new Date();
+//     // return <h1>{new Date().toLocaleTimeString()}</h1>;
+
+//     // const inputType = 'number'
+//     // const minValue = 5;
+//     // return <input
+//     //     style={{border: '3px solid red'}}
+//     //     type={inputType}
+//     //     min={5}
+//     //     max={9969}
+//     // />;
+
+//     return <textarea spellCheck="false" autoFocus={true} style={{border: '3px solid red', width: '100%', height: '500px'}}/>;
+// }
+
+////////////////// 5) Show the component on the screen ////////////////////////////////////////////////////////////////////////
+
+root.render(<App/>)
